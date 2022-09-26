1ce5161d464685bce823b1cb406dae0b544cc0f8
Anton Savenkov <a_savi@icloud.com>
2022-09-26 08:03:30 +0300

    add task 19

--- Seminare003/task19/Program.cs
+++ Seminare003/task19/Program.cs
@@ -5,5 +5,34 @@
 12821 -> да
 23432 -> да
 */
+int getNumbeerFromUser(string userInform)
+{
+    int result = 0;
+    Console.Write($"{userInform} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 9999 || result > 99999)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInform} ");
+    }
+    return result;
+}
+int numberFromUser = getNumbeerFromUser("Введите пятизначное число ");
+numberFromUser = Convert.ToInt32(numberFromUser);
 
+Console.WriteLine(numberFromUser);
 
+bool CheckNumberForPalindrome(int number)
+{
+    bool result = false;
+    if (number / 10000 == number % 10)
+    {
+        if ((number / 1000) % 10 == (number % 100) / 10)
+        {
+            result = true;
+        }
+    }
+    return result;
+}
+bool checkNumberForPalindrome = CheckNumberForPalindrome(numberFromUser);
+Console.WriteLine(checkNumberForPalindrome);
+
+Console.WriteLine(checkNumberForPalindrome ? $"Число {numberFromUser} является палиндромом" : $"Число {numberFromUser} не является палиндромом"); 
