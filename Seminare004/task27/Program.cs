05719a0a50dd6a9a2604c5f644ec79c1210981a5
Anton Savenkov <a_savi@icloud.com>
2022-09-29 19:25:33 +0300

    add task 27

--- Seminare004/task27/Program.cs
+++ Seminare004/task27/Program.cs
@@ -4,3 +4,18 @@
 82 -> 10
 9012 -> 12
 */
+
+int sumOfDigitInNumber(int number)
+{
+    int summ = 0;
+    for (int i = 1; number != 0; i++)
+    {
+        summ = summ + (number % 10);
+        number = number / 10;
+    }
+    summ = Math.Abs(summ);
+    return summ;
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} равна {sumOfDigitInNumber(number)}");
