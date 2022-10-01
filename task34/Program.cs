ab7ee353ab18e1e2a64469bee91a5f32782cba19
Anton Savenkov <a_savi@icloud.com>
2022-10-01 13:12:19 +0300

    removed excess from task 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -3,10 +3,18 @@
 [345, 897, 568, 234] -> 2
 */
 
-int randomNumberWithin(int minNum, int maxNum)
+int randomNumberWithin(int minNum, int maxNum) // случайное число в заданном диапазоне
 {
     int randomNumber = new Random().Next(100, 999);
     return randomNumber;
+}
+
+void FillAray(int[] newArray) // заполнение массива
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = randomNumberWithin(100, 1000);
+    }
 }
 
 void printArray(int[] arrayRandom) //вывод массива в консоль
@@ -18,6 +26,7 @@ void printArray(int[] arrayRandom) //вывод массива в консоль
     }
     Console.Write($"{arrayRandom[arrayRandom.Length-1]}]");
 }
+
 int searchForEvenNumbers(int[] arrayRandomNumbers) // поиск четных элементов массива
 {
     int countEvenNumbers = 0;
@@ -30,13 +39,11 @@ int searchForEvenNumbers(int[] arrayRandomNumbers) // поиск четных э
     }
     return countEvenNumbers;
 }
+
 Console.WriteLine("Задайте длинну массива: ");
 int userLength = Convert.ToInt32(Console.ReadLine());
-int[] arrayRandom = new int [userLength];
-for (int i = 0; i < arrayRandom.Length; i++)
-{
-    arrayRandom[i] = randomNumberWithin(100, 1000);
-}
-printArray(arrayRandom);
-Console.Write($" колличество четных элементов = {searchForEvenNumbers(arrayRandom)}");
+int[] arrayRandomNum = new int [userLength];
+FillAray(arrayRandomNum);
+printArray(arrayRandomNum);
+Console.Write($" колличество четных элементов = {searchForEvenNumbers(arrayRandomNum)}");
 Console.WriteLine("");
---
+++ task36/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+void printArray(int[] arrayRandomNum)
+{
+    Console.Write($"[");
+    for (int i = 0; i < arrayRandomNum.Length - 1; i++)
+    {
+        Console.Write($" {arrayRandomNum[i]},");
+    }
+    Console.Write($" {arrayRandomNum[arrayRandomNum.Length - 1]}]");
+}
+void fillAray(int[] newArr)
+{
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        newArr[i] = new Random().Next(1, 100);
+    }
+}
+
+
+int[] newArray = new int[10];
+printArray(newArray);
+fillAray(newArray);
+printArray(newArray);
