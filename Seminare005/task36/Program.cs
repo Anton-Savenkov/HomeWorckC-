b656873be919e254420484a67e9351c20eb32d66
Anton Savenkov <a_savi@icloud.com>
2022-10-02 12:11:05 +0300

    streamline task in seminare 5

---
+++ Seminare005/task36/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+void PrintArray(int[] arrayRandomNum)
+{
+    Console.Write($"[");
+    for (int i = 0; i < arrayRandomNum.Length - 1; i++)
+    {
+        Console.Write($"{arrayRandomNum[i]}, ");
+    }
+    Console.Write($"{arrayRandomNum[arrayRandomNum.Length - 1]}]");
+}
+
+void FillAray(int[] newArr)
+{
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        newArr[i] = new Random().Next(1, 10);
+    }
+}
+int SumElementByIndex(int[] newArr)
+{
+    int summElements = 0;
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        if (i % 2 != 0) summElements += newArr[i];
+    }
+    return summElements;
+}
+
+int[] newArray = new int[10];
+// PrintArray(newArray);
+FillAray(newArray);
+PrintArray(newArray);
+Console.WriteLine($" Сумма элементов на нечетных позициях = {SumElementByIndex(newArray)}");
---
+++ Seminare005/task37/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 37(сложная необязательная): Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+void PrintArray(int[] arrayNew)
+{
+    Console.Write($"[");
+    for(int i = 0; i < arrayNew.Length - 1; i++)
+    {
+        Console.Write($"{arrayNew[i]}, ");
+    }
+    Console.Write($"{arrayNew[arrayNew.Length - 1]}]");
+}
+
+void FillAray(int[] arrayNew)
+{
+    for(int i = 0; i < arrayNew.Length; i++)
+    {
+        arrayNew[i] = new Random().Next(1, 5);
+    }
+}
+
+void MultiPairsNumber(int[] arrayNew)
+{
+    int multiPairsNumber = 0;
+    
+    for(int i = 0; i < arrayNew.Length / 2; i++)
+    {
+        multiPairsNumber = arrayNew[i] * arrayNew[arrayNew.Length - i - 1];
+        Console.Write($" {multiPairsNumber}");
+    }
+    
+}
+    
+
+int[] arrayRandomNum = new int [7];
+FillAray(arrayRandomNum);
+Console.Write("Заданный массив: ");
+PrintArray(arrayRandomNum);
+Console.Write("Произведение пар чисел равны: ");
+MultiPairsNumber(arrayRandomNum);
+Console.WriteLine();
