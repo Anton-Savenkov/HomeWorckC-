5aa762945049b1303097447bf3ca930456b6ff7a
Anton Savenkov <a_savi@icloud.com>
2022-10-09 21:47:14 +0300

    add task 47 & task 50

---
+++ Seminare007/task50/Program.cs
@@ -0,0 +1,93 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позицию элемента в двумерном массиве, и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+5 -> 9
+индексы:
+[0][1][2][3]
+1 4 7 2
+[4][5][6][7]
+5 9 2 3
+[8][9][10][11]
+8 4 2 4
+*/
+
+int[,] generate2DArray(int height,int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height,width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i,j] = new Random().Next(randomStart, randomEnd+1 );
+        }
+    }
+    return twoDArray;
+}
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i+"\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i+"\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void searchElementInOrder(int[,] random2DArray, int searchOrderNum, int height, int width)
+{
+    int result = 0;
+    int countNumber = 0;
+    if(searchOrderNum <= height * width - 1)
+    {
+        for (int i = 0; i < random2DArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < random2DArray.GetLength(0); j++)
+            {
+                countNumber ++;
+                if(countNumber - 1 == searchOrderNum)
+                { 
+                    result = random2DArray[i,j];
+                    Console.WriteLine($"{searchOrderNum} -> {result}");
+                    return;
+                }
+                
+            }
+        }
+    }
+    else Console.WriteLine($"{searchOrderNum} -> такого числа в массиве нет");
+}
+
+int[,] generatedArray = generate2DArray(3,3,0,30);
+print2DArray(generatedArray);
+Console.WriteLine("Введите позицию элемента для поиска: ");
+int searchOrderNum = Convert.ToInt32(Console.ReadLine());
+searchElementInOrder(generatedArray, searchOrderNum, 3, 3);
+
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
