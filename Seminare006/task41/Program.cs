c86962dc5feca8a60a343a52b60b8b26a8806823
Anton Savenkov <a_savi@icloud.com>
2022-10-09 13:53:45 +0300

    add task 43

---
+++ Seminare006/task41/Program.cs
@@ -0,0 +1,41 @@
+﻿/*  Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Введите несколько чисел через робел: ");
+int[] inputUseerArray = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+int CountNumber(int[] inputUserArr)
+{
+    int coutNumber = 0;
+    for (int i = 0; i < inputUserArr.Length; i++)
+    {
+        if(inputUserArr[i] > 0)
+        coutNumber += 1;
+    }
+    return coutNumber;
+}
+
+void printArray(int[] arrayToPrint, string name)
+{
+    System.Console.WriteLine($"---{name}----");
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
+printArray(inputUseerArray, "Введеннные числа");
+Console.WriteLine($"Колличество чисел больше 0 -> {CountNumber(inputUseerArray)}");
+
+
+/*
+*Задача(сложная не на оценку) *
+Напишите программу, котрая находит подмножество данного множества чисел такое, что сумма его элементов равна заданному числу
+*/
---
+++ Seminare006/task43/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double coordinateXPointOfIntersecTwoLine(double k1, double b1, double k2, double b2)
+{
+    double coordinateXPoint = (b1 - b2) / (k2 - k1);
+    return coordinateXPoint;
+}
+double coordinateYPointOfIntersecTwoLine(double coordinateX, double k2, double b2)
+{
+    double coordinateYPoint = k2 * coordinateX + b2;
+    return coordinateYPoint;
+}
+
+Console.WriteLine("Введите значения переменных:");
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double coordinateXPointOfIntersec = coordinateXPointOfIntersecTwoLine(k1, b1, k2, b2);
+double coordinateYPointOfIntersec = coordinateYPointOfIntersecTwoLine(coordinateXPointOfIntersec, k2, b2);
+Console.WriteLine($"Точка пересечения двух заданных прямых -> [{coordinateXPointOfIntersec}; {coordinateYPointOfIntersec}]");
