c16ac64dcad6f2f510b355d71d1f169058037662
Anton Savenkov <a_savi@icloud.com>
2022-10-12 23:15:49 +0300

    add task 52 & screen task 47,50,52

--- Seminare007/task52/Program.cs
+++ Seminare007/task52/Program.cs
@@ -18,9 +18,21 @@ int[,] generate2DArray(int height,int width, int randomStart, int randomEnd)
     }
     return twoDArray;
 }
-void printColorData(string data)
+void printColorDataDarkGreen(string data)
 {
-    Console.ForegroundColor = ConsoleColor.Green;
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void printColorDataBlue(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void printColorDataRed(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
     Console.Write(data);
     Console.ResetColor();
 }
@@ -29,12 +41,12 @@ void print2DArray(int[,] arrayToPrint)
     Console.Write(" \t");
     for (int i = 0; i < arrayToPrint.GetLength(1); i++)
     {
-        printColorData(i+"\t");
+        printColorDataRed(i+"\t");
     }
     Console.WriteLine();
     for (int i = 0; i < arrayToPrint.GetLength(0); i++)
     {
-        printColorData(i+"\t");
+        printColorDataDarkGreen(i+"\t");
         for (int j = 0; j < arrayToPrint.GetLength(1); j++)
         {
             Console.Write(arrayToPrint[i,j] + "\t");
@@ -42,22 +54,55 @@ void print2DArray(int[,] arrayToPrint)
         Console.WriteLine();
     }
 }
-void averageСolumn(int[,] random2DArray, int height, int width)
+void printIntArray(int[] inputArray)
 {
-    double[] averageСolumnArr = new double[height];
-    int i = 0;
-    while(random2DArray.GetLength(0) == i)
+    Console.Write("\t");
+    for (int i = 0; i < inputArray.Length; i++)
     {
-        averageСolumnArr[i] = random2DArray[i,j];
+        printColorDataRed(inputArray[i] + "\t");
     }
 }
-Console.WriteLine("Введите размер массива M x N,");
-Console.Write("M = ");
-int height = Convert.ToInt32(Console.ReadLine());
-Console.Write("N = ");
-int width = Convert.ToInt32(Console.ReadLine());
-int[,] generatedArray = generate2DArray(3,3,0,30);
+void printDoubleArray(double[] inputArray)
+{
+    Console.Write("\t");
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        printColorDataBlue(inputArray[i] + "\t");
+    }
+}
+int[] sumInColumns(int[,] input2DArray)
+{
+    int[] sumInColumnsArr = new int [input2DArray.GetLength(0)];
+    for (int i = 0; i < input2DArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < input2DArray.GetLength(1); j++)
+        {
+            sumInColumnsArr[j] += input2DArray[i,j];
+        }
+    }
+    return sumInColumnsArr;
+}
+double[] columnsAverage(int [] inputArray, int width)
+{
+    double widthDouble = Convert.ToDouble(width);
+    double[] columnsAverArr = new double [inputArray.Length];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        columnsAverArr[i] = inputArray[i] / widthDouble;
+    }
+    return columnsAverArr;
+}
+
+int height = 5;
+int width = 5;
+int[,] generatedArray = generate2DArray(height,width,0,20);
+Console.Clear();
 print2DArray(generatedArray);
-Console.WriteLine("Введите позицию элемента для поиска: ");
-int searchOrderNum = Convert.ToInt32(Console.ReadLine());
-searchElementInOrder(generatedArray, searchOrderNum, height, width);
+int[] sumInColumnsArray = sumInColumns(generatedArray);
+Console.WriteLine();
+printIntArray(sumInColumnsArray);
+double[] columnsAverArray = columnsAverage(sumInColumnsArray,width);
+Console.WriteLine();
+Console.WriteLine("--- Среднее арифметическое каждого столбца ---");
+printDoubleArray(columnsAverArray);
+Console.WriteLine();
