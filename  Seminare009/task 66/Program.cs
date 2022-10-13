6efbffeb33a04d2f87f10a662dbdd3c10e127f7d
Anton Savenkov <a_savi@icloud.com>
2022-10-13 23:31:19 +0300

    add task 66

---  Seminare009/task 66/Program.cs
+++  Seminare009/task 66/Program.cs
@@ -3,3 +3,20 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+Console.Write("Введите число M = ");
+int userNumberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N = ");
+int userNumberN = Convert.ToInt32(Console.ReadLine());
+int summNumbersFromMToN(int numberM, int numberN)
+{
+    if(numberM == numberN + 1)
+    {
+        return 0;
+    }
+    int summ = numberN + summNumbersFromMToN(numberM, numberN - 1);
+    return summ;
+}
+
+int summNum = summNumbersFromMToN(userNumberM, userNumberN);
+Console.Write($"Сумма чисел = {summNum}");
+Console.WriteLine();
